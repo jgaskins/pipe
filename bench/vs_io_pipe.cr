dc58575cf78decc6af9e25ad4091abb204af94e8
Jamie Gaskins <jgaskins@hey.com>
2026-01-06 23:26:46 -0500

    Benchmark against many different buffer sizes

--- bench/vs_io_pipe.cr
+++ bench/vs_io_pipe.cr
@@ -1,18 +1,28 @@
 require "benchmark"
 require "../src/pipe"
 
-Benchmark.ips do |x|
-  io_reader, io_writer = IO.pipe
-  pipe_reader, pipe_writer = Pipe.create
-  bytes = Bytes.new(1024)
+[
+  8,
+  100,
+  1024,
+  8192,
+  65536,
+].each do |byte_size|
+  puts
+  puts byte_size.humanize_bytes
+  Benchmark.ips do |x|
+    io_reader, io_writer = IO.pipe
+    pipe_reader, pipe_writer = Pipe.create
+    bytes = Bytes.new(byte_size)
 
-  x.report "IO.pipe" do
-    io_writer.write bytes
-    io_reader.read bytes
-  end
+    x.report "IO.pipe" do
+      io_writer.write bytes
+      io_reader.read bytes
+    end
 
-  x.report "Pipe.create" do
-    pipe_writer.write bytes
-    pipe_reader.read bytes
+    x.report "Pipe.create" do
+      pipe_writer.write bytes
+      pipe_reader.read bytes
+    end
   end
 end
